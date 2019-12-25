0903fd7fb799e747f226445fdd743c23b9d0ce58
ICYPOLE <39186199+ICYPOLE@users.noreply.github.com>
2019-12-26 00:39:24 +0800

    Updated router

--- src/router/index.js
+++ src/router/index.js
@@ -5,17 +5,15 @@ import Router from 'vue-router'
 import Login from '@/views/Login.vue'
 import MainPage from '@/views/mainPage.vue'
 import ProfilePage from '@/views/profilePage.vue'
+import TiePage from '@/views/tiePage.vue'
+import BlockPage from '@/views/blockPage.vue'
+import TieZi from '@/views/tiezi.vue'
 
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'HelloWorld',
-    //   component: HelloWorld
-    // },
     {
       path: '/',
       name: 'Login',
@@ -24,7 +22,22 @@ export default new Router({
     {
       path: '/MainPage',
       name: 'MainPage',
-      componet: MainPage
+      component: MainPage
+    },
+    {
+      path: '/BlockPage/:forum_id',
+      name: 'BlockPage',
+      component: BlockPage
+    },
+    {
+      path: '/TiePage/:forum_id',
+      name: 'TiePage',
+      component: TiePage
+    },
+    {
+      path: '/TieZi/:topic_id',
+      name: 'TieZi',
+      component: TieZi
     },
     {
       path: '/ProfilePage',
